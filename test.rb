87e18c1910b99f9e55b9b5d0a80ff03a87cc7f02
Alek Seibel <alek.seibel@gmail.com>
2022-04-11 13:15:38 -0400

    added classes

--- test.rb
+++ test.rb
@@ -1,20 +1,82 @@
-class Cards
-    @@deck_cards = Array.new(52)
+class Card
 
-    def initialize()
-        #Create cards types in cards deck instance variable
+    def initialize(suite, value)
+        @suite = suite
+        @value = value
+    end
+
+    def getSuite()
+        return @suite
+    end 
+
+    def getValue()
+        return @value
     end
 end
+
+class Deck
+
+    def initialize()
+        @cards = Array.new(52)
+    end
+
+    #Creates a deck of cards, ensuring random ordering, Fills the instance array
+    def createDeck()
+    end
+
+    #Gets a card from the deck, removes it, and return the Card Object
+    def getCard()
+    end
+end
+
+class Player
+
+    def initialize(betValue)
+        @betValue = betValue
+        @cardHand = CardHand.new()
+
+    def hold()
+    end
+
+    def hit()
+    end
+
+    def bet(betValue)
+    end
+
+end
+
+class Dealer
+
+    def initialize(deckNumber)
+        @playingDeck = Array.new(deckNumber)
+    end
+
+    def deal()
+    end
+end
+
+class CardHand
+
+    def initialize()
+        @cardsInHand = Array.new(20)
+    end
+
+    def addCard(card)
+    end
+end
+
 
 class GameController
-    cards = Cards.new;
-    def initialize()
+
+    def initialize(numberOfPlayers, numberOfCardDecks)
+        @numberOfPlayers = numberOfPlayers
+        @numberOfCardDecks = numberOfCardDecks
     end
 
-    def main()
-        puts "The Game is Beginning!";
+    def run()
+    end
+
+    def win_loss()
     end
 end
-
-myGame = GameController.new;
-myGame.main;
